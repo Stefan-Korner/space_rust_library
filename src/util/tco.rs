c5dae279302ac6f259f0bfa6f2f72472ab7809ad
Stefan-Korner <korner-hajek@gmx.at>
2018-09-09 22:54:59 +0200

    change time correlation to time::Duration calculations

--- src/util/tco.rs
+++ src/util/tco.rs
@@ -17,40 +17,43 @@
 // OBT...Onboard Time line                                                    *
 //                                                                            *
 // The module is statically configured via entries in config.txt              *
+// Calculation are base on time::Timespec (from Crate time)                   *
 //*****************************************************************************
 
+use time;
+use util::asd_time;
 use util::config;
 
 ///////////////
 // constants //
 ///////////////
-// TAI_MISSION_EPOCH = "1958.001.00.00.00.000"
-static TAI_MISSION_EPOCH_DELTA: i32 = -378691200;
-// UNIX_MISSION_EPOCH = "1970.001.00.00.00.000"
-static UNIX_MISSION_EPOCH_DELTA: i32 = 0;
-// GPS_MISSION_EPOCH = "1980.006.00.00.00.000"
-static GPS_MISSION_EPOCH_DELTA: i32 = 315964800;
-static GPS_LEAP_SECONDS_1980: i32 = 0;
-static GPS_LEAP_SECONDS_2009: i32 = 15;
-static GPS_LEAP_SECONDS_2012: i32 = 16;
-static GPS_LEAP_SECONDS_2015: i32 = 17;
-static GPS_LEAP_SECONDS_2017: i32 = 18;
+pub static TAI_MISSION_EPOCH: &str = "1958.001.00.00.00.000";
+pub static TAI_MISSION_EPOCH_DELTA: i64 = -378691200;
+pub static UNIX_MISSION_EPOCH: &str = "1970.001.00.00.00.000";
+pub static UNIX_MISSION_EPOCH_DELTA: i64 = 0;
+pub static GPS_MISSION_EPOCH: &str = "1980.006.00.00.00.000";
+pub static GPS_MISSION_EPOCH_DELTA: i64 = 315964800;
+pub static GPS_LEAP_SECONDS_1980: i64 = 0;
+pub static GPS_LEAP_SECONDS_2009: i64 = 15;
+pub static GPS_LEAP_SECONDS_2012: i64 = 16;
+pub static GPS_LEAP_SECONDS_2015: i64 = 17;
+pub static GPS_LEAP_SECONDS_2017: i64 = 18;
 
 lazy_static! {
     static ref ERT_LEAP_SECONDS_STR: String =
         config::get_config_value_as_string("TCO_ERT_LEAP_SECONDS_STR");
     static ref ERT_MISSION_EPOCH_STR: String =
         config::get_config_value_as_string("TCO_ERT_MISSION_EPOCH_STR");
-    static ref ERT_MISSION_EPOCH_WITH_LEAP_SECONDS: i32 =
-        get_mission_epoch_with_leap_seconds(
+    static ref ERT_DELTA: time::Duration =
+        get_mission_epoch_with_leap_seconds_delta(
             &ERT_MISSION_EPOCH_STR,
             &ERT_LEAP_SECONDS_STR);
     static ref OBT_LEAP_SECONDS_STR: String =
         config::get_config_value_as_string("TCO_OBT_LEAP_SECONDS_STR");
     static ref OBT_MISSION_EPOCH_STR: String =
         config::get_config_value_as_string("TCO_OBT_MISSION_EPOCH_STR");
-    static ref OBT_MISSION_EPOCH_WITH_LEAP_SECONDS: i32 =
-        get_mission_epoch_with_leap_seconds(
+    static ref OBT_DELTA: time::Duration =
+        get_mission_epoch_with_leap_seconds_delta(
             &OBT_MISSION_EPOCH_STR,
             &OBT_LEAP_SECONDS_STR);
 }
@@ -59,37 +62,48 @@ lazy_static! {
 // functions //
 ///////////////
 
-// converts the leap seconds string to seconds
-fn get_leap_seconds(leap_seconds_str: &str) -> i32 {
-    match leap_seconds_str {
+// converts a leap seconds string (either symbolic or numeric) to seconds
+fn get_leap_seconds(leap_seconds_str: &str) -> time::Duration {
+    let leap_seconds = match leap_seconds_str {
         "GPS_LEAP_SECONDS_1980" => GPS_LEAP_SECONDS_1980,
         "GPS_LEAP_SECONDS_2009" => GPS_LEAP_SECONDS_2009,
         "GPS_LEAP_SECONDS_2012" => GPS_LEAP_SECONDS_2012,
         "GPS_LEAP_SECONDS_2015" => GPS_LEAP_SECONDS_2015,
         "GPS_LEAP_SECONDS_2017" => GPS_LEAP_SECONDS_2017,
         _ => {
-            leap_seconds_str.parse::<i32>().expect(
+            leap_seconds_str.parse::<i64>().expect(
                 &format!("Leap seconds string {} has invalid symbolic format", leap_seconds_str))
-        }
-    }
+        },
+    };
+    time::Duration::seconds(leap_seconds)
 }
 
-// converts the mission epoch string to seconds
-fn get_mission_epoch(mission_epoch_str: &str) -> i32 {
-    match mission_epoch_str {
-        "TAI_MISSION_EPOCH" => return TAI_MISSION_EPOCH_DELTA,
-        "UNIX_MISSION_EPOCH" => return UNIX_MISSION_EPOCH_DELTA,
-        "GPS_MISSION_EPOCH" => return GPS_MISSION_EPOCH_DELTA,
-        _ => {}
+// converts a mission epoch string (either symbolic or ASD format) to seconds
+fn get_mission_epoch(mission_epoch_str: &str) -> time::Duration {
+    let timespec = match mission_epoch_str {
+        "TAI_MISSION_EPOCH" => {
+            asd_time::get_time(TAI_MISSION_EPOCH_DELTA, 0)
+         },
+        "UNIX_MISSION_EPOCH" => {
+            asd_time::get_time(UNIX_MISSION_EPOCH_DELTA, 0)
+         },
+        "GPS_MISSION_EPOCH" => {
+            asd_time::get_time(GPS_MISSION_EPOCH_DELTA, 0)
+         },
+        _ => {
+            asd_time::parse_time(mission_epoch_str).expect(
+                &format!("Mission epoch string {} has invalid symbolic format", mission_epoch_str))
+        },
     };
-    1234
+    // convert Timespec to Duration
+    timespec - time::Timespec::new(0, 0)
 }
 
 // converts mission epoch string and leap seconds string
 // into mission epoch in seconds
-fn get_mission_epoch_with_leap_seconds(
+fn get_mission_epoch_with_leap_seconds_delta (
     mission_epoch_str: &str,
-    leap_seconds_str: &str) -> i32 {
+    leap_seconds_str: &str) -> time::Duration {
     get_mission_epoch(mission_epoch_str) - get_leap_seconds(leap_seconds_str)
 }
 
@@ -97,32 +111,36 @@ fn get_mission_epoch_with_leap_seconds(
 // accessors //
 ///////////////
 
-// return the ERT mission epoch incl. leap seconds
-pub fn get_ert_mission_epoch_with_leap_seconds() -> i32 {
-    *ERT_MISSION_EPOCH_WITH_LEAP_SECONDS
+// return the ERT delta
+pub fn get_ert_delta() -> time::Duration {
+    *ERT_DELTA
 }
 
 // correlate the ERT mission epoch time to the local time
-pub fn correlate_from_ert_mission_epoch(rs_epoch_time: u32) -> u32 {
-    ((rs_epoch_time as i32) + *ERT_MISSION_EPOCH_WITH_LEAP_SECONDS) as u32
+pub fn correlate_from_ert_mission_epoch(epoch_time: time::Timespec) ->
+    time::Timespec {
+    epoch_time + *ERT_DELTA
 }
 
 // correlate the local time to ERT mission epoch time
-pub fn correlate_to_ert_mission_epoch(rs_utc_time: u32) -> u32 {
-    ((rs_utc_time as i32) - *ERT_MISSION_EPOCH_WITH_LEAP_SECONDS) as u32
+pub fn correlate_to_ert_mission_epoch(unix_time: time::Timespec) ->
+    time::Timespec {
+    unix_time - *ERT_DELTA
 }
 
-// return the OBT mission epoch incl. leap seconds
-pub fn get_obt_mission_epoch_with_leap_seconds() -> i32 {
-    *OBT_MISSION_EPOCH_WITH_LEAP_SECONDS
+// return the OBT delta
+pub fn get_obt_delta() -> time::Duration {
+    *OBT_DELTA
 }
 
 // correlate the OBT mission epoch time to the local time
-pub fn correlate_from_obt_mission_epoch(rs_epoch_time: u32) -> u32 {
-    ((rs_epoch_time as i32) + *OBT_MISSION_EPOCH_WITH_LEAP_SECONDS) as u32
+pub fn correlate_from_obt_mission_epoch(epoch_time: time::Timespec) ->
+    time::Timespec {
+    epoch_time + *OBT_DELTA
 }
 
 // correlate the local time to OBT mission epoch time
-pub fn correlate_to_obt_mission_epoch(rs_utc_time: u32) -> u32 {
-    ((rs_utc_time as i32) - *OBT_MISSION_EPOCH_WITH_LEAP_SECONDS) as u32
+pub fn correlate_to_obt_mission_epoch(unix_time: time::Timespec) ->
+    time::Timespec {
+    unix_time - *OBT_DELTA
 }
--- src/util_tests/tco_tests.rs
+++ src/util_tests/tco_tests.rs
@@ -13,6 +13,7 @@
 // Utilities - Time Correlation - Unit tests                                  *
 //*****************************************************************************
 
+use util::asd_time;
 use util::tco;
 
 ///////////////
@@ -20,10 +21,36 @@ use util::tco;
 ///////////////
 
 pub fn test() {
-    println!(
-        "ert_mission_epoch_with_leap_seconds = {}",
-        tco::get_ert_mission_epoch_with_leap_seconds());
-    println!(
-        "obt_mission_epoch_with_leap_seconds = {}",
-        tco::get_obt_mission_epoch_with_leap_seconds());
+    println!("ERT delta = {}", tco::get_ert_delta().num_seconds());
+    println!("OBT delta = {}", tco::get_obt_delta().num_seconds());
+    let tai_zero = asd_time::parse_time(tco::TAI_MISSION_EPOCH).unwrap();
+    let unix_zero = asd_time::parse_time(tco::UNIX_MISSION_EPOCH).unwrap();
+    let gps_zero = asd_time::parse_time(tco::GPS_MISSION_EPOCH).unwrap();
+    println!("TAI zero = {}", asd_time::get_time_str_with_nano(tai_zero));
+    println!("UNIX zero = {}", asd_time::get_time_str_with_nano(unix_zero));
+    println!("GPS zero = {}", asd_time::get_time_str_with_nano(gps_zero));
+    let tai_ert_correlated = tco::correlate_to_ert_mission_epoch(tai_zero);
+    let unix_ert_correlated = tco::correlate_to_ert_mission_epoch(unix_zero);
+    let gps_ert_correlated = tco::correlate_to_ert_mission_epoch(gps_zero);
+    let tai_obt_correlated = tco::correlate_to_obt_mission_epoch(tai_zero);
+    let unix_obt_correlated = tco::correlate_to_obt_mission_epoch(unix_zero);
+    let gps_obt_correlated = tco::correlate_to_obt_mission_epoch(gps_zero);
+    println!("TAI ERT correlated to = {}", asd_time::get_time_str_with_nano(tai_ert_correlated));
+    println!("UNIX ERT correlated to = {}", asd_time::get_time_str_with_nano(unix_ert_correlated));
+    println!("GPS ERT correlated to = {}", asd_time::get_time_str_with_nano(gps_ert_correlated));
+    println!("TAI OBT correlated to = {}", asd_time::get_time_str_with_nano(tai_obt_correlated));
+    println!("UNIX OBT correlated to = {}", asd_time::get_time_str_with_nano(unix_obt_correlated));
+    println!("GPS OBT correlated to = {}", asd_time::get_time_str_with_nano(gps_obt_correlated));
+    let tai_ert_correlated = tco::correlate_from_ert_mission_epoch(tai_ert_correlated);
+    let unix_ert_correlated = tco::correlate_from_ert_mission_epoch(unix_ert_correlated);
+    let gps_ert_correlated = tco::correlate_from_ert_mission_epoch(gps_ert_correlated);
+    let tai_obt_correlated = tco::correlate_from_obt_mission_epoch(tai_obt_correlated);
+    let unix_obt_correlated = tco::correlate_from_obt_mission_epoch(unix_obt_correlated);
+    let gps_obt_correlated = tco::correlate_from_obt_mission_epoch(gps_obt_correlated);
+    println!("TAI ERT correlated from = {}", asd_time::get_time_str_with_nano(tai_ert_correlated));
+    println!("UNIX ERT correlated from = {}", asd_time::get_time_str_with_nano(unix_ert_correlated));
+    println!("GPS ERT correlated from = {}", asd_time::get_time_str_with_nano(gps_ert_correlated));
+    println!("TAI OBT correlated from = {}", asd_time::get_time_str_with_nano(tai_obt_correlated));
+    println!("UNIX OBT correlated from = {}", asd_time::get_time_str_with_nano(unix_obt_correlated));
+    println!("GPS OBT correlated from = {}", asd_time::get_time_str_with_nano(gps_obt_correlated));
 }
